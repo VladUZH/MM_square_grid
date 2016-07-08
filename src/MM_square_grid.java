4144aa33bdbaafc8b8304b0ac698cc24b423354e
Vladimir Petrov <vladimir.petrov@lykkex.com>
2016-07-08 11:01:55 +0200

    Now one can run the code only for some part of agents with different delta_up and delta_down

--- src/MM_square_grid.java
+++ src/MM_square_grid.java
@@ -26,45 +26,52 @@ public class MM_square_grid {
 
         MM mm = new MM(ALPHA);
 
+        int nIterations = 50;
 
-        double[] averagePrices = new double[N_GENERATIONS];
+        for (int lastY = 0; lastY < N_GENERATIONS - 1; lastY++) {
 
-        int nIterations = 100;
-        for (int iteration = 0; iteration < nIterations; iteration++) {
-            System.out.println("Iteration " + iteration + " started");
-
-            double[] priceList = new double[N_GENERATIONS];
+            double[] averagePrices = new double[N_GENERATIONS];
 
 
-            ATick aTick = new ATick(START_PRICE);
+            for (int iteration = 0; iteration < nIterations; iteration++) {
+                System.out.println("Iteration " + iteration + " started");
 
-            int listIndex = 0;
-            for (int aGeneration = 0; aGeneration < N_GENERATIONS; aGeneration++) {
-                int exceedVolume = 0;
-                for (int stepX = 0; stepX < N_DELTAS; stepX++) {
-                    for (int stepY = 0; stepY < N_DELTAS; stepY++) {
-                        if (traders[stepX][stepY].runner.deltaUp > traders[stepX][stepY].runner.deltaDown){
-                            exceedVolume += (traders[stepX][stepY].runTrading(aTick));
+                double[] priceList = new double[N_GENERATIONS];
+
+
+                ATick aTick = new ATick(START_PRICE);
+
+                int listIndex = 0;
+                for (int aGeneration = 0; aGeneration < N_GENERATIONS; aGeneration++) {
+                    int exceedVolume = 0;
+                    for (int stepX = 0; stepX < N_DELTAS; stepX++) {
+                        for (int stepY = 0; stepY < N_DELTAS; stepY++) {
+                            if (stepY * (N_DELTAS - 1) <= stepX * lastY) {
+                                exceedVolume += (traders[stepX][stepY].runTrading(aTick));
+                            }
                         }
                     }
-                }
-                double newPrice = mm.generateNextPrice(aTick.price, exceedVolume, ALPHA);
+                    double newPrice = mm.generateNextPrice(aTick.price, exceedVolume, ALPHA);
 //                System.out.println(newPrice);
-                priceList[listIndex] = newPrice;
-                aTick = new ATick(newPrice);
-                listIndex++;
+                    priceList[listIndex] = newPrice;
+                    aTick = new ATick(newPrice);
+                    listIndex++;
+                }
+
+                for (listIndex = 0; listIndex < N_GENERATIONS; listIndex++) {
+                    averagePrices[listIndex] += priceList[listIndex];
+                }
+
+
             }
 
-            for (listIndex = 0; listIndex < N_GENERATIONS; listIndex++){
-                averagePrices[listIndex] += priceList[listIndex];
+            for (int listIndex = 0; listIndex < N_GENERATIONS; listIndex++){
+                System.out.println(averagePrices[listIndex] / nIterations);
             }
-
 
         }
 
-        for (int listIndex = 0; listIndex < N_GENERATIONS; listIndex++){
-            System.out.println(averagePrices[listIndex] / nIterations);
-        }
+
 
 
     }
