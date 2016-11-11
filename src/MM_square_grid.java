818fee7cee65b1dd0411b9221f28732b36d743b8
Vladimir Petrov <vladimir.petrov@lykkex.com>
2016-11-11 10:00:12 +0100

    Now there is a preliminary "warming up" file.

--- src/MM_square_grid.java
+++ src/MM_square_grid.java
@@ -3,7 +3,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -16,14 +20,18 @@ public class MM_square_grid {
     public static final int N_DELTAS = 50;
 
     public static final String FILE_PATH = "D:/Data/OANDA/Positions/";
+    public static DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    public static int nDecimal = 4;
 
 
 
 
     public static void main(String[] args){
 
-//        String fileName = "OANDA_historical_position_ratios_data_EUR_USD.csv";
-        String fileName = "OANDA_historical_position_ratios_data_USD_CHF.csv";
+        String fileName = "OANDA_historical_position_ratios_data_EUR_USD.csv";
+        String trainFileName = "EURUSD_2015_daily.csv";
+//        String fileName = "OANDA_historical_position_ratios_data_USD_CHF.csv";
 
         ATick aTick;
 
@@ -37,17 +45,71 @@ public class MM_square_grid {
             }
         }
 
+
+
+
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_PATH + fileName));
 
             String thisLine = bufferedReader.readLine(); // header
+            thisLine = bufferedReader.readLine(); // first string
+
+            long firstRealDate = Long.valueOf(thisLine.split(",")[0]);
+
+
+            // in this part we train agents by a real data one year before the OANDA data:
+
+            try {
+                BufferedReader trainBufferedReader = new BufferedReader(new FileReader(FILE_PATH + "../../" + trainFileName));
+
+                String line = trainBufferedReader.readLine();
+                String[] elements;
+
+                boolean notEnd = true;
+
+                while (notEnd){
+                    elements = trainBufferedReader.readLine().split(",");
+
+
+                    int intPrice = (int) (Double.parseDouble(elements[1]) * Math.pow(10, nDecimal));
+                    aTick = new ATick(intPrice);
+                    for (int stepX = 0; stepX < N_DELTAS; stepX++) {
+                        for (int stepY = 0; stepY < N_DELTAS; stepY++) {
+//                        if (stepX == stepY){ // "<" - I region, ">" - III region, "==" - II region
+                            if (true) {
+                                traders1[stepX][stepY].runTrading(aTick);
+                            }
+                        }
+                    }
+
+
+                    Date date = dateFormat.parse(elements[0]);
+
+                    System.out.println(elements[0]);
+
+                    if (date.after(new Date(firstRealDate * 1000L))){
+                        notEnd = false;
+                    }
+
+                }
+
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+
+            // in the following part we use the same data which OANDA show:
+
+
+
+
+
+
+
 
             String line;
             String[] components;
 
-            boolean firstString = true;
 
-            int nDecimal = 0;
 
             String outFileName = "RESULT_" + fileName;
             PrintWriter writer = new PrintWriter("Results/" + outFileName, "UTF-8");
@@ -56,10 +118,6 @@ public class MM_square_grid {
             while ((line = bufferedReader.readLine()) != null) {
                 components = line.split(",");
 
-                if (firstString){
-                    nDecimal = 4;
-                    firstString = false;
-                }
 
                 int intPrice = (int) (Double.parseDouble(components[2]) * Math.pow(10, nDecimal));
 
